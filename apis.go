af40deb1b06c1bc4c6c6aa8dcd7d5e707166e404
ZwzGithub <1158636266@qq.com>
2021-07-06 15:48:34 +0800

    Add files via upload

---
+++ apis.go
@@ -0,0 +1,15 @@
+/**
+ * @Author: AS
+ * @Description:
+ * @File:  apis
+ * @Version: 1.0.0
+ * @Date: 2021/07/05 11:02
+ */
+
+package main
+
+import "fmt"
+
+func Says(str string) {
+	fmt.Println("hello", str)
+}
